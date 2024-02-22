1da6a483c02523a74673cc621ad1c84f542e8842
cbeetler <71681083+cbeetler@users.noreply.github.com>
2024-02-22 12:30:24 -0500

    project completion

---
+++ components/Input.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Input(props) {
+  const { type = "text", placeholder, name } = props;
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      name={name}
+      className="ui-textfield"
+    />
+  );
+}
---
+++ index.jsx
@@ -0,0 +1,24 @@
+// packages
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import React from "react";
+
+// components
+import Link from "./components/Link.jsx";
+import Button from "./components/Button.jsx";
+import Container from "./components/Container.jsx";
+import Input from "./components/Input.jsx";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Link href="https://react-tutorial.app">React Tutorial</Link>
+        <Button disabled={true}>Login</Button>
+        <Input placeholder="Full Name" name="full_name" />
+      </Container>
+    </>
+  );
+}
+
+createRoot(document.querySelector("#react-root")).render(<App />);
